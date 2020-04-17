94418049faeec37cd3ba17cba4152a7d5c3be2a1
matiassanchez12 <52495199+matiassanchez12@users.noreply.github.com>
2020-04-17 19:28:48 -0300

    Add files via upload

---
+++ TP 1 laboratorio/utn.h
@@ -0,0 +1,22 @@
+
+
+/** \brief solicita un número al usuario y devuelve el resultado
+ *
+ * \param mensaje: es el mensaje que pide ingresar datos
+ * \return devuelve el numero ingresado
+ */
+int getInt (char mensaje[]);
+
+/** \brief solicita un número flotante al usuario y devuelve el resultado
+ *
+ * \param mensaje: es el mensaje que pide ingresar datos
+ * \return devuelve el numero flotante ingresado
+ */
+float getFloat(char mensaje[]);
+
+/** \brief solicita una letra al usuario y devuelve el resultado
+ *
+ * \param mensaje: es el mensaje que pide ingresar datos
+ * \return devuelve la letra ingresada
+ */
+char getChar(char mensaje[]);
