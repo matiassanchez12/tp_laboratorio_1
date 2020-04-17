2d5eec1afc7f791d82d28ae6fcc1a349ad38a896
matiassanchez12 <52495199+matiassanchez12@users.noreply.github.com>
2020-04-17 16:24:08 -0300

    Add files via upload

---
+++ TP 1 laboratorio/Funciones.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+#include "funciones.h"
+
+int menuPrincipal(float num1, float num2, int flag, int flag1)
+{
+    int opcion;
+    system("cls");
+    printf(">>Menu de Opciones de la calculadora:\n");
+
+    if(flag == 1)
+    {
+        printf("\n 1. Ingresar 1er operando (A = %.2f)", num1);
+    }
+    else
+    {
+        printf("\n 1. Ingresar 1er operando (A = x)");
+    }
+
+    if(flag1 == 1)
+    {
+        printf("\n 2. Ingresar 2do operando (B = %.2f)", num2);
+    }
+    else
+    {
+        printf("\n 2. Ingresar 2do operando (B = y)");
+    }
+
+    printf("\n 3. Calcular todas las operaciones");
+    printf("\n  a) Calcular la suma (A+B)");
+    printf("\n  b) Calcular la resta (A-B)");
+    printf("\n  c) Calcular la division (A/B)");
+    printf("\n  d) Calcular la multiplicacion (A*B)");
+    printf("\n  e) Calcular el factorial (A!) y (B!)");
+    printf("\n 4. Informar resultados");
+    printf("\n 5. Salir");
+
+    fflush(stdin);
+    opcion = getInt("\n\nIngrese una opcion: ");
+    return opcion;
+}
+
+void informarResultados(float num1,float num2, float rSum, float rRest, float rDiv, float rMult, int rFact1, int rFact2)
+{
+    printf("\n\t a) El resultado de %.2f + %.2f es: %.2f", num1, num2, rSum);
+    printf("\n\t b) El resultado de %.2f - %.2f es: %.2f", num1, num2, rRest);
+
+    if (num2 != 0)
+    {
+        printf("\n\t c) El resultado de %.2f / %.2f es: %.2f", num1, num2, rDiv);
+    }
+    else
+    {
+        printf("\n\t No es posible dividir por cero");
+    }
+
+    printf("\n\t d) El resultado de %.2f * %.2f es: %.2f", num1, num2, rMult);
+
+    if((rFact1 < 0) || (rFact2 < 0))
+    {
+        printf("\n \t No se pudo calcular factorial. Ingresar numeros enteros mayores a 0 y menores a 32 para esta operacion.");
+    }
+    else
+    {
+        printf("\n\t e) El resultado de %.2f! es: %d y %.2f! es: %d", num1, rFact1, num2, rFact2);
+    }
+
+    printf("\n\n");
+}
+
+float suma(float num1, float num2)
+{
+    float resultado;
+
+    resultado = num1 + num2;
+
+    return resultado;
+}
+
+float resta(float num1, float num2)
+{
+    float resultado;
+
+    resultado = num1 - num2;
+
+    return resultado;
+}
+
+float division(float num1, float num2)
+{
+    float resultado;
+
+    resultado = num1 / num2;
+
+    return resultado;
+}
+
+float multiplicar(float num1, float num2)
+{
+    float resultado;
+
+    resultado = num1 * num2;
+
+    return resultado;
+}
+
+int factorial(float a)
+{
+    int i, aux;
+    int resultado = 1;
+    aux = (int)a;
+
+    if(a != aux || a < 0)
+    {
+        return -1;
+    }
+    else
+    {
+
+        for(i=1 ; i<=aux; i++)
+        {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
+}
--- TP 1 laboratorio/main.c
+++ TP 1 laboratorio/main.c
@@ -1,8 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Funciones.h"
+#include "utn.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    int opcion, rFactorial1, rFactorial2;
+    float rSuma, rResta, rDivision, rMultiplicacion, numA, numB;
+    int flag = 0, flag1 = 0, flag2 = 0, exit = 0;
+
+    do
+    {
+        opcion = menuPrincipal(numA, numB, flag1, flag2);
+        switch (opcion)
+        {
+        case 1 :
+            numA = getFloat("Ingrese numero A: "); ///Se pide ingresar operando A
+            flag1 = 1;
+            break;
+        case 2:
+            numB = getFloat("Ingrese un numero B: "); ///Se pide ingresar operando B
+            flag2 = 1;
+            break;
+        case 3:
+            if(flag1 == 1 && flag2 == 1) ///Si se ingresaron numeros A Y B, entonces se calcularan los datos
+            {
+                rSuma = suma(numA, numB);
+
+                rResta = resta(numA, numB);
+
+                rDivision = division(numA, numB);
+
+                rMultiplicacion = multiplicar(numA, numB);
+
+                rFactorial1 = factorial(numA);
+                rFactorial2 = factorial(numB);
+
+                printf("\n*Operaciones calculadas con exito*\n");
+                flag = 1;
+            }
+            else ///No hay datos para calcular, se muestra un mensaje de error
+            {
+                printf("\nError. No ingreso ningun operando o solo uno.\n");
+            }
+            break;
+        case 4:
+            if(flag != 1) ///Si no se ingresaron numeros A y B o no se ingreso calcular datos, no muestra calculos.
+            {
+                printf("\nError. No ingreso operandos o no ingreso a la opcion 3 para calcular.\n");
+            }
+            else    ///Si se ingresaron numeros, junto a la opcion - 3. calcular - ,se muestra menu de resultados
+            {
+                informarResultados(numA, numB, rSuma,rResta, rDivision, rMultiplicacion, rFactorial1, rFactorial2);
+            }
+            break;
+        case 5:
+            printf("\nSaliendo de la calculadora. Que tenga buen dia!\n");
+            exit = 1;
+            break;
+        default:
+            printf("\nError. Ingrese 1 opcion del menu.\n");
+            break;
+        }
+        system("pause");
+    }
+    while(exit == 0);
+
     return 0;
 }
